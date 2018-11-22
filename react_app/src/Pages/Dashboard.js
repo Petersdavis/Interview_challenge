ab3ba08343c9a1a1be1ef2b32040d6bf7005cc0c
Peter Davis <pdavis@lifelearn.com>
2018-11-22 15:11:34 -0500

    Added stubs for 2 layouts

---
+++ react_app/src/Pages/Dashboard.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import {Row, Col} from 'react-bootstrap';
+
+
+class Dashboard extends Component{
+    constructor(props) {
+        super(props);
+        this.logout = this.logout.bind(this);
+
+    }
+
+    componentDidMount() {
+        document.title = "Crypt-Toe-Verse";
+    }
+
+    logout(){
+        this.props.logout()
+    }
+
+    render(){
+
+        return(
+            <div className = "about">
+                <div style={{backgroundImage:"url("+Intro+")"}} className= "intro-chunk">
+                    <div className="breadcrumbs"><a aria-label="home page" onClick={this.goToHomePage} className="breadcrumb-home">Home</a><div className="breadcrumb-page">About</div></div>
+
+                    <Row>
+                        <Col xs={6}>
+                            <h1>{this.contentParser("headline")}</h1>
+                            <p>{this.contentParser("headline-text")}</p>
+                        </Col>
+
+                    </Row>
+
+                </div>
+                <div className="chunk-grey">
+                    <h2>{this.contentParser("dechra-headline")}</h2>
+                    <p>{this.contentParser("dechra-text")}</p>
+
+                    <h2>{this.contentParser("history-headline")}</h2>
+                    <p>{this.contentParser("history-text")}</p>
+
+                    <a href={this.contentParser("visit-dechra-url")} className="action-item">{this.contentParser("visit-dechra")}</a>
+
+                </div>
+
+            </div>
+        )
+
+
+
+
+    }
+}
+
+export default Dashboard;
