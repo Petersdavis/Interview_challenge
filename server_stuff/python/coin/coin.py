122de7f57201e590fa88e66c4c51973e442415ec
Peter Davis <pdavis@lifelearn.com>
2018-11-26 22:52:24 -0500

    added scripts for python to sync and store data.

--- server_stuff/python/coin/coin.py
+++ server_stuff/python/coin/coin.py
@@ -2,12 +2,12 @@ import requests
 import json
 
 class Coin:
-    def listAll():
+    def listAll(self):
         r = requests.get('https://api.coinmarketcap.com/v2/listings/')
         return r.json()
     
     
-    def getSome(self, start_at: int = 1):
+    def getSome(self, start_at):
         url = 'https://api.coinmarketcap.com/v2/ticker/?start=' + str(start_at) + "&sort=id"
         r = requests.get(url)
         return r.json()
